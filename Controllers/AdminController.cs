2a68ce5b697e6e444219b73efa91e2ae7a63ae57
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2020-01-22 08:55:54 -0400

    Cambio a otro repositorio publico y con .ignore de vs

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Userregister()
+        {
+            return View();
+        }
+        public IActionResult Useredit()
+        {
+            return View();
+        }
+        public IActionResult User()
+        {
+            return View();
+        }
+    }
+}
