f9f28bbea74945bc5b0174f9c20c309e2e53745f
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2020-01-22 14:09:55 -0400

    Some views

---
+++ Controllers/DocsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema.Controllers
+{
+    public class DocsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DocCreate()
+        {
+            return View();
+        }
+    }
+}
